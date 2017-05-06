839c2133d829820131fe7f4e21a0235872d8ece3
Li Zhe <lizhe@lizhe.org>
2017-05-06 13:10:46 +0800

    add route

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var API_HOST = 'anarpishtifunc.azurewebsites.net';
+
 var BRANDS = [{
     name: '石榴熟了',
     slag: 'anar',
@@ -8,22 +9,62 @@ var BRANDS = [{
     slag: 'qipar',
     selected: 0
 }];
+
 var app = new Vue({
     el: '#anar_app',
     data: {
-        brands: BRANDS,
-        recentVideos: {
+        brands: {
+            data: BRANDS
+        },
+        videoList: {
             data: []
+        },
+        path: {
+            data: 'home'
         }
     },
     methods: {
         getRecentVideos: getRecentVideos,
         transformNumberUnderHundred: transformNumberUnderHundred,
         selectSeason: selectSeason,
-        toDateString: toDateString
+        toDateString: toDateString,
+        getBrandVideos: getBrandVideos
     },
-    created: getRecentVideos
+    created: function() {
+        window.onhashchange = handlePath;
+        setTimeout(handlePath, 0);
+    }
 });
+
+function handlePath() {
+    var routes = [{
+        do: app.getRecentVideos
+    }, {
+        path: '/video/(.*?)',
+        do: function(matches) {
+            app.getBrandVideos(matches[1]);
+        }
+    }];
+
+    var matched = false;
+    var path = location.hash;
+    path = path ? path.substr(1) : '';
+    routes.forEach(function(route) {
+        if (matched) {
+            return;
+        }
+        
+        if (route.path && new RegExp('^' + route.path + '$').test(path)) {
+            route.do(path.match(new RegExp('^' + route.path + '$')));
+            app.$set(app.path, 'data', path);
+            matched = true;
+        }
+    });
+    if (!matched) {
+        app.$set(app.path, 'data', 'home');
+        routes[0].do();
+    }
+}
 
 function getRecentVideos() {
     var url = 'https://' + API_HOST + '/api/video/get';
@@ -35,7 +76,11 @@ function getRecentVideos() {
             });
             videoList.push(brandVideos);
         });
-        app.$set(app.recentVideos, 'data', videoList);
+        BRANDS.forEach(function(brand) {
+            brand.selected = 0;
+        });
+        app.$set(app.brands, 'data', BRANDS);
+        app.$set(app.videoList, 'data', videoList);
     }).fail(function(error) {
 
     });
@@ -62,7 +107,7 @@ function transformNumberUnderHundred(num) {
 }
 
 function selectSeason(brandIndex, seasonIndex) {
-    this.brands[brandIndex].selected = seasonIndex;
+    this.brands.data[brandIndex].selected = seasonIndex;
 }
 
 function isNewVideo(time) {
@@ -77,4 +122,24 @@ function toDateString(timespan) {
     month = month < 10 ? ('0' + month) : month;
     day = day < 10 ? ('0' + day) : day;
     return year + '.' + month + '.' + day;
+}
+
+function getBrandVideos(brand) {
+    var url = 'https://' + API_HOST + '/api/video/get?brand=' + brand;
+    jQuery.get(url).done(function(res) {
+        var videoList = [];
+        res.data.sort(function(a, b){
+            return b[0].season - a[0].season;
+        });
+        videoList.push(res.data);
+        BRANDS.forEach(function(b) {
+            if (b.slag === brand) {
+                b.selected = 0;
+                app.$set(app.brands, 'data', [b]);
+            }
+        });
+        app.$set(app.videoList, 'data', videoList);
+    }).fail(function(error) {
+
+    });
 }
